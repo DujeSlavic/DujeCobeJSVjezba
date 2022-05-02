9a14c5a4a475d8636d124dcb55440ee4ad69036c
DujeSlavic <99210500+DujeSlavic@users.noreply.github.com>
2022-05-02 21:25:59 +0200

    change

--- partThreeJS/script3.js
+++ partThreeJS/script3.js
@@ -3,18 +3,29 @@ const article1 = document.getElementById('article1');
 const article2 = document.getElementById('article2');
 const article3 = document.getElementById('article3');
 
-let articles = 0;
-//document.getElementById('submitForm').disabled = true;
+let inBasket = 0;
 
-form.addEventListener('submit', (e) =>{
-    e.preventDefault();
+let numberOfArticles = (id) => {
+    let array = document.getElementById(id).innerHTML.split(':');
+    return parseInt(array[1]) || 0;
+}
+
+let ValidateEmail = (mail) => {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) return true;
+    
+    document.getElementById('errorMail').innerHTML = 'Invalid email!';
+    return false;
+}
+
+form.addEventListener('submit', (event) =>{
+    event.preventDefault();
 
     const userName = document.getElementById('userName').value;
     const mail = document.getElementById('userEmail').value;
     const message = document.getElementById('userMessage').value;
     const delivery = document.getElementById('freeDelivery').checked;
     
-    if(userName && ValidateEmail(mail) && articles > 0){
+    if(userName && ValidateEmail(mail) && inBasket > 0){
         console.log('name: ', userName);
         console.log('email: ', mail);
         console.log('message: ', message);
@@ -25,7 +36,20 @@ form.addEventListener('submit', (e) =>{
     }
 })
 
+let addArticle = (id) => {
+    let array = document.getElementById(id).innerHTML.split(':');
+    let number = parseInt(array[1]) || 0;
+    number++;
+    inBasket++;
+    document.getElementById(id).innerHTML = 'Quantity:' + number;
+}
 
+
+article1.onclick = addArticle.bind(this,'labelA1');
+article2.onclick = addArticle.bind(this,'labelA2');
+article3.onclick = addArticle.bind(this,'labelA3');
+
+/*
 function ValidateEmail(mail) 
 {
  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
@@ -36,23 +60,11 @@ function ValidateEmail(mail)
     document.getElementById('errorMail').innerHTML = 'Invalid email!';
     return (false)
 }
+*/
 
-
+/*
 function numberOfArticles(id){
     let array = document.getElementById(id).innerHTML.split(':');
     return parseInt(array[1]) || 0;
 }
-
-
-let addArticle = (id) => {
-    let array = document.getElementById(id).innerHTML.split(':');
-    let number = parseInt(array[1]) || 0;
-    number++;
-    articles++;
-    document.getElementById(id).innerHTML = 'Quantity:' + number;
-}
-
-
-article1.onclick = addArticle.bind(this,'labelA1');
-article2.onclick = addArticle.bind(this,'labelA2');
-article3.onclick = addArticle.bind(this,'labelA3');
+*/
